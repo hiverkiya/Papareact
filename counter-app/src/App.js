f2724bef5974e7466e1a003615cb40bfc4b666ed
hiverkiya <hiverkiya@gmail.com>
2023-06-18 16:56:35 +0530

    Counter app state

---
+++ counter-app/src/App.js
@@ -0,0 +1,21 @@
+import {useState} from "react"
+
+function App() {
+  const [count,setCount]=useState(0);
+  function incre(){
+    setCount(count+1);
+  }
+  function decre(){
+    setCount(count-1);
+  }
+  return (
+    <div className="App">
+      <p> hello</p>
+      <p> Count = {count}</p>
+      <button onClick={incre}>+</button>
+      <button onClick={decre}>-</button>
+    </div>
+  );
+}
+
+export default App;
