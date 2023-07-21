e6cfdba26758d110f2cf92df9cd6e7c95a018b3d
hiverkiya <hiverkiya@gmail.com>
2023-07-21 15:02:05 +0530

    Testing with Cypress

---
+++ cypress-testing/cypress/e2e/app.cy.ts
@@ -0,0 +1,31 @@
+/* eslint-disable */
+// Disable ESLint to prevent failing linting inside the Next.js repo.
+// If you're using ESLint on your project, we recommend installing the ESLint Cypress plugin instead:
+// https://github.com/cypress-io/eslint-plugin-cypress
+
+// Cypress E2E Test
+describe('Navigation', () => {
+  it('should navigate to the about page', () => {
+    // Start from the index page
+    cy.visit('http://localhost:3000/')
+
+    // Find a link with an href attribute containing "about" and click it
+    cy.get('a[href*="about"]').click()
+
+    // The new url should include "/about"
+    cy.url().should('include', '/about')
+
+    // The new page should contain an h1 with "About page"
+    cy.get('h1').contains('About Page')
+  })
+
+  it("should visit the home page from the about page",()=>{
+  cy.visit("http://localhost:3000/about")
+  cy.get("[data-cy=go-back-button]").click();
+  cy.url().should("eq","http://localhost:3000/")
+  cy.get('[data-cy=title-home-page]').contains("Next.js!")
+  })
+})
+
+// Prevent TypeScript from reading file as legacy script
+export {}
