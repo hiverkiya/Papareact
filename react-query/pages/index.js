449842479189f8ac1868d815f57c5702ae478293
hiverkiya <hiverkiya@gmail.com>
2023-08-17 22:08:44 +0530

    react query

--- react-query/pages/index.js
+++ react-query/pages/index.js
@@ -1,16 +1,26 @@
 import Image from "next/image";
-import { useQuery } from "react-query";
+import { useQuery, useQueryClient } from "react-query";
 
 export default function Home() {
-  const { isLoading, error, data } = useQuery("repoData", () =>
-    fetch("https://api.github.com/repos/tannerlinsley/react-query").then(
-      (res) => res.json()
-    )
-  );
+  // const { isLoading, error, data } = useQuery("repoData", () =>
+  //   fetch("https://api.github.com/repos/tannerlinsley/react-query").then(
+  //     (res) => res.json()
+  //   )
+  // );
+  const queryClient = useQueryClient();
+  const { isLoadingTodo, errorTodo, data } = useQuery("todos", () => [
+    "Himanshu",
+  ]);
+
+  const postTodo = (e) => {
+    e.preventDefault();
+    queryClient.setQueryData("todos", [...data, "Take dogs out"]);
+  };
+
   return (
     <div className=" flex flex-col items-center justify-center min-h-screen py-2">
       <div>Hello</div>
-      {isLoading ? (
+      {/* {isLoading ? (
         <h1> The content is loading</h1>
       ) : (
         <>
@@ -22,7 +32,16 @@ export default function Home() {
             <strong>🍴 {data.forks_count}</strong>
           </div>
         </>
-      )}
+      )} */}
+
+      <div>
+        {" "}
+        <h1>TODO</h1>
+        {data?.map((todo) => (
+          <p>{todo}</p>
+        ))}
+        <button onClick={postTodo}>Add a Todo</button>
+      </div>
     </div>
   );
 }
