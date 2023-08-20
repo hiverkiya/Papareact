51e406b1d749e37446aa60e3251a2e5cd9f14ce7
hiverkiya <hiverkiya@gmail.com>
2023-08-20 16:36:57 +0530

    react hooks form

---
+++ react-hooks-form/pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col justify-center items-center py-2">
+      <div className="text-4xl"> React Hooks Form</div>
+    </div>
+  );
+}
