7e942613c77323b612f8f369ff3612643a25e4ee
hiverkiya <hiverkiya@gmail.com>
2023-09-13 21:25:09 +0530

    Project finalized

--- google-sheet-api/src/App.js
+++ google-sheet-api/src/App.js
@@ -1,10 +1,12 @@
 import axios from "axios";
+import "./App.css";
 import { Button, TextField } from "@material-ui/core";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useForm } from "react-hook-form";
 function App() {
   const { register, handleSubmit, watch, errors } = useForm();
-
+  const [tableData, setTableData] = useState([]);
+  const [totSal, setTotSal] = useState(0);
   const submitFormToGoogle = (data) => {
     console.log(data);
     console.log("Submitted");
@@ -22,6 +24,8 @@ function App() {
       )
       .then((response) => {
         alert("Row added");
+        // fetchData();
+        setTableData([...tableData, data]);
         console.log(response);
       });
   };
@@ -29,8 +33,28 @@ function App() {
   // const ageInputRef = useRef(null);
   // const salaryInputRef = useRef(null);
   // const hobbyInputRef = useRef(null);
+  useEffect(() => {
+    fetchData();
+    const total = tableData.reduce(
+      (total, row) => (total = total + Number(row.salary)),
+      0
+    );
+    setTotSal(total);
+  }, [tableData]);
+
+  const fetchData = () => {
+    axios
+      .get("https://sheet.best/api/sheets/4b65d61f-6899-4058-b3e6-9ec9e18c8774")
+      .then((response) => {
+        console.log("Google sheets data>>>", response);
+        setTableData(response.data);
+      });
+  };
+
+  ////////////////////////////////////////////////////
+
   return (
-    <div>
+    <div className="app">
       <div> Hello</div>
 
       <form onSubmit={handleSubmit(submitFormToGoogle)}>
@@ -60,6 +84,21 @@ function App() {
         />{" "}
         <Button type="submit">Submit</Button>
       </form>
+      <div>
+        <table>
+          {tableData.map(({ age, hobby, name, salary }) => (
+            <tr>
+              <td>{name}</td>
+              <td>{age}</td>
+              <td>${salary}</td>
+              <td>{hobby}</td>
+            </tr>
+          ))}
+        </table>
+      </div>
+      <div>
+        <h2> Grand Total of salaries : ${totSal}</h2>
+      </div>
     </div>
   );
 }
