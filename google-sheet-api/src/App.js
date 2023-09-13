ccec6e75d3065f3e1ec75ae54b1894099005b6c1
hiverkiya <hiverkiya@gmail.com>
2023-09-13 15:53:45 +0530

    Fixing react hook form

--- google-sheet-api/src/App.js
+++ google-sheet-api/src/App.js
@@ -1,39 +1,63 @@
 import axios from "axios";
 import { Button, TextField } from "@material-ui/core";
 import { useRef, useState } from "react";
+import { useForm } from "react-hook-form";
 function App() {
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const { register, handleSubmit, watch, errors } = useForm();
+
+  const submitFormToGoogle = (data) => {
+    console.log(data);
     console.log("Submitted");
+    const { name, age, salary, hobby } = data;
     const tableData = {
-      name: nameInputRef.current.value,
-      age: ageInputRef.current.value,
-      salary: salaryInputRef.current.value,
-      hobby: hobbyInputRef.current.value,
+      name,
+      age,
+      salary,
+      hobby,
     };
-    console.log(tableData);
     axios
       .post(
         "https://sheet.best/api/sheets/4b65d61f-6899-4058-b3e6-9ec9e18c8774",
         tableData
       )
       .then((response) => {
+        alert("Row added");
         console.log(response);
       });
   };
-  const nameInputRef = useRef(null);
-  const ageInputRef = useRef(null);
-  const salaryInputRef = useRef(null);
-  const hobbyInputRef = useRef(null);
+  // const nameInputRef = useRef(null);
+  // const ageInputRef = useRef(null);
+  // const salaryInputRef = useRef(null);
+  // const hobbyInputRef = useRef(null);
   return (
     <div>
       <div> Hello</div>
 
-      <form onSubmit={handleSubmit}>
-        <TextField inputRef={nameInputRef} variant="filled" label="Name" />{" "}
-        <TextField inputRef={ageInputRef} variant="filled" label="Age" />{" "}
-        <TextField variant="filled" inputRef={salaryInputRef} label="Salary" />{" "}
-        <TextField variant="filled" inputRef={hobbyInputRef} label="Hobby" />{" "}
+      <form onSubmit={handleSubmit(submitFormToGoogle)}>
+        <TextField
+          name="name"
+          {...register("name")}
+          variant="filled"
+          label="Name"
+        />{" "}
+        <TextField
+          name="age"
+          {...register("age")}
+          variant="filled"
+          label="Age"
+        />{" "}
+        <TextField
+          name="salary"
+          variant="filled"
+          {...register("salary")}
+          label="Salary"
+        />{" "}
+        <TextField
+          name="hobby"
+          variant="filled"
+          {...register("hobby")}
+          label="Hobby"
+        />{" "}
         <Button type="submit">Submit</Button>
       </form>
     </div>
