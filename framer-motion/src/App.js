9a64d314dba5df8d8c9fc5ef54e9d01848dc5759
hiverkiya <hiverkiya@gmail.com>
2023-09-14 18:02:51 +0530

    Implemented an example

--- framer-motion/src/App.js
+++ framer-motion/src/App.js
@@ -1,7 +1,23 @@
+import { useState } from "react";
+import "./App.css";
+import { motion } from "framer-motion";
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div>
+    <div className="app">
       <div> Framer Motion</div>
+      <button onClick={() => setCount(count + 1)}>
+        Click to restart the animation
+      </button>
+      <div className="cont">
+        <motion.div
+          key={count}
+          initial={{ scale: 0 }}
+          animate={{ scale: 1 }}
+          transition={{ duration: 0.5 }}
+        />
+      </div>
     </div>
   );
 }
